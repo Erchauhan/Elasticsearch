79c59211de6b9d87fd218c8e67d9fd2d65ad3814
Erchauhan <chauhanprerana.mit@gmail.com>
2020-02-24 16:08:23 +0530

    update main.tf

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -1,10 +1,3 @@
-provider "azurerm" {
-  subscription_id = "${var.azure_subscription_id}"
-  client_id = "${var.azure_client_id}"
-  client_secret = "${var.azure_client_secret}"
-  tenant_id = "${var.azure_tenant_id}"
-}
-
 resource "random_string" "vm-login-password" {
   length = 16
   special = true
